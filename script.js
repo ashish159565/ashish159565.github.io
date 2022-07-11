cf5d3e340edd37f6c8a0b8b37bdb697482bcd99e
ASHISH GAJJELA <ashishgajjala@rediffmail.com>
2022-07-11 10:49:33 +0530

    adding buttons

--- script.js
+++ script.js
@@ -43,3 +43,60 @@ for (var i = 0; i < navLinks.length; i++) {
         menuBtn.style.pointerEvents = "auto";
     });
 }
+
+const buttons = document.querySelectorAll("main button");  
+buttons.forEach((button) => {  
+     button.addEventListener("pointerenter", handleMouseEnter);  
+     button.addEventListener("pointerleave", handleMouseLeave);  
+});  
+document.body.addEventListener("pointermove", updateCursorPosition);  
+document.body.addEventListener("pointerdown", () => {  
+     gsap.to(cursorInner, 0.15, {  
+          scale: 2,  
+     });  
+});  
+document.body.addEventListener("pointerup", () => {  
+     gsap.to(cursorInner, 0.15, {  
+          scale: 1,  
+     });  
+});  
+function updateCursorPosition(e) {  
+     mouse.x = e.pageX;  
+     mouse.y = e.pageY;  
+}  
+function updateCursor() {  
+     gsap.set(cursorInner, {  
+          x: mouse.x,  
+          y: mouse.y,  
+     });  
+     if (!isStuck) {  
+          gsap.to(cursorOuter, {  
+               duration: 0.15,  
+               x: mouse.x - cursorOuterOriginalState.width/2,  
+               y: mouse.y - cursorOuterOriginalState.height/2,  
+          });  
+     }  
+     requestAnimationFrame(updateCursor);  
+}  
+updateCursor();  
+function handleMouseEnter(e) {  
+     isStuck = true;  
+     const targetBox = e.currentTarget.getBoundingClientRect();  
+     gsap.to(cursorOuter, 0.2, {  
+          x: targetBox.left,   
+          y: targetBox.top + scrollHeight,  
+          width: targetBox.width,  
+          height: targetBox.width,  
+          borderRadius: 0,  
+          backgroundColor: "rgba(255, 255, 255, 0.1)",  
+     });  
+}  
+function handleMouseLeave(e) {  
+     isStuck = false;  
+     gsap.to(cursorOuter, 0.2, {  
+          width: cursorOuterOriginalState.width,  
+          height: cursorOuterOriginalState.width,  
+          borderRadius: "50%",  
+          backgroundColor: "transparent",  
+     });  
+}
